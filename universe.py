a942a6c7ef9b3c93dc97a13e46b2c431418f7f1d
emond777-prog <emond777@gmail.com>
2026-04-23 22:40:48 -0400

    Create universe.py

---
+++ universe.py
@@ -0,0 +1,15 @@
+def get_universe():
+    return [
+        # US (liquid options)
+        "AAPL","MSFT","GOOGL","AMZN","META","NVDA","TSLA",
+        "AMD","NFLX","INTC","SPY","QQQ","IWM",
+
+        # Financials
+        "JPM","BAC","GS","MS",
+
+        # Energy
+        "XOM","CVX",
+
+        # Canada (TSX)
+        "SHOP.TO","RY.TO","TD.TO","BNS.TO","ENB.TO"
+    ]
