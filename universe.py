4de43fe2b4f015f72e59064e627d718b6da44e57
emond777-prog <emond777@gmail.com>
2026-04-24 00:09:36 -0400

    Update universe.py

--- universe.py
+++ universe.py
@@ -2,47 +2,36 @@ def get_universe():
     return [
 
     # ===== MEGA CAPS =====
-    "AAPL","MSFT","GOOGL","AMZN","META","NVDA","TSLA","BRK-B","AVGO","LLY",
-    "V","MA","UNH","XOM","JNJ","WMT","JPM","PG","HD","COST",
+    "AAPL","MSFT","GOOGL","AMZN","META","NVDA","TSLA","AVGO","LLY","V",
+    "MA","UNH","XOM","JNJ","WMT","JPM","PG","HD","COST","MRK",
 
     # ===== TECH =====
     "AMD","INTC","CSCO","ADBE","CRM","ORCL","QCOM","TXN","NOW","PANW",
     "SNOW","PLTR","SHOP","UBER","SQ","NET","DDOG","ZS","CRWD","MDB",
 
     # ===== FINANCIALS =====
-    "JPM","BAC","WFC","C","GS","MS","BLK","SCHW","AXP","USB",
-    "PNC","TFC","COF","BK","STT","CME","ICE","SPGI","MCO","AON",
+    "BAC","WFC","C","GS","MS","BLK","SCHW","AXP","USB","PNC",
+    "TFC","COF","CME","ICE","SPGI","MCO","AON",
 
     # ===== HEALTHCARE =====
-    "JNJ","PFE","MRK","ABBV","LLY","TMO","ABT","DHR","BMY","AMGN",
-    "GILD","ISRG","SYK","ZTS","REGN","VRTX","BDX","EW","DXCM","IDXX",
+    "PFE","ABBV","TMO","ABT","DHR","BMY","AMGN","GILD","ISRG","SYK",
+    "ZTS","REGN","VRTX","DXCM",
 
     # ===== ENERGY =====
-    "XOM","CVX","COP","EOG","SLB","PSX","VLO","MPC","KMI","OKE",
-    "HAL","DVN","FANG","BKR","APA","OXY","WMB","TRGP","EQT","PXD",
+    "CVX","COP","EOG","SLB","PSX","VLO","MPC","KMI","OKE","HAL",
 
     # ===== CONSUMER =====
-    "AMZN","TSLA","HD","MCD","NKE","SBUX","LOW","TGT","COST","WMT",
-    "ROST","TJX","YUM","CMG","DG","DLTR","ULTA","EBAY","ETSY","MAR",
+    "MCD","NKE","SBUX","LOW","TGT","ROST","TJX","YUM","CMG","DG",
 
     # ===== INDUSTRIALS =====
-    "BA","CAT","GE","HON","UPS","FDX","LMT","RTX","DE","MMM",
-    "NOC","GD","EMR","ETN","PH","ROK","TT","CMI","PCAR","OTIS",
+    "BA","CAT","GE","HON","UPS","FDX","LMT","RTX","DE","ETN",
 
-    # ===== TELECOM =====
-    "T","VZ","TMUS","CHTR","CMCSA","DIS","NFLX","PARA","WBD","FOX",
+    # ===== TELECOM / MEDIA =====
+    "T","VZ","TMUS","CMCSA","DIS","NFLX",
 
-    # ===== ETFs (IMPORTANT) =====
+    # ===== ETFs (VERY IMPORTANT) =====
     "SPY","QQQ","IWM","DIA","XLF","XLE","XLK","XLV","XLY","XLI",
-    "ARKK","SMH","SOXX","VTI","VOO","IVV","VEA","VWO","EFA","EEM",
 
-    # ===== ADDITIONAL LIQUID NAMES =====
-    "PYPL","SQ","AFRM","COIN","HOOD","RIVN","LCID","NKLA","SOFI","UPST",
-    "F","GM","RCL","CCL","UAL","DAL","AAL","LUV","EXPE","BKNG",
-    "ORLY","AZO","KMX","CVNA","TSCO","LEN","DHI","NVR","PHM","WY",
-    "PLD","AMT","CCI","EQIX","PSA","O","SPG","VICI","DLR","SBAC",
-    "NEE","DUK","SO","AEP","EXC","XEL","ED","PEG","EIX","PCG",
-
-    # ===== CANADA (OPTIONAL BUT GOOD) =====
-    "SHOP.TO","RY.TO","TD.TO","BNS.TO","ENB.TO","CNQ.TO","SU.TO","BMO.TO","TRP.TO","CP.TO"
+    # ===== HIGH LIQUID EXTRA =====
+    "PYPL","COIN","HOOD","RIVN","SOFI","UPST","F","GM","DAL","UAL"
     ]
