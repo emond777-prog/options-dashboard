14ba51673c4448b3a5b35a92de745ff67e73aa5b
emond777-prog <emond777@gmail.com>
2026-04-23 22:57:16 -0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -6,4 +6,14 @@ def load_positions():
     try:
         return pd.read_csv(FILE)
     except:
-        return pd.DataFrame()
+        return pd.DataFrame(columns=[
+            "ticker","type","strike","expiry",
+            "premium","contracts","entry_price","current_price"
+        ])
+
+
+def save_position(new_row):
+    df = load_positions()
+
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    df.to_csv(FILE, index=False)
