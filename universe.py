6d822449a3457e632ed0a6589d6ccd1fe7fd2b14
emond777-prog <emond777@gmail.com>
2026-04-23 23:46:32 -0400

    Update universe.py

--- universe.py
+++ universe.py
@@ -1,15 +1,7 @@
-import pandas as pd
-
 def get_universe():
-    try:
-        url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-        tables = pd.read_html(url)
-        sp500 = tables[0]
-
-        tickers = sp500["Symbol"].tolist()
-        tickers = [t.replace(".", "-") for t in tickers]
-
-        return tickers
-
-    except:
-        return ["AAPL","MSFT","GOOGL","AMZN","META","NVDA","TSLA"]
+    return [
+        "AAPL","MSFT","GOOGL","AMZN","META","NVDA","TSLA",
+        "AMD","NFLX","INTC","CSCO","ADBE","CRM","ORCL","QCOM",
+        "AVGO","TXN","PYPL","SHOP","UBER","SNOW","PLTR",
+        "SPY","QQQ","IWM"
+    ]
